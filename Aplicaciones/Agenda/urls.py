b664034ab55225cd9df70c06cb77e4c97a6bfe5c
Adrian-Troya <wilmer.troya1680@utc.edu.ec>
2025-03-19 12:47:31 -0500

    Subiendo las rutas de login y reset

--- Aplicaciones/Agenda/urls.py
+++ Aplicaciones/Agenda/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home/',views.home, name='home'),
-
-    # CRUDS  
+    path('', views.login, name='login'),
+    path('home/', views.home, name='home'),  # Ruta para la página principal
+    path('reset/', views.reset, name='reset'),
+    # otras rutas...
 ]
