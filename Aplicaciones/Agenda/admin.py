e2627fe2b66adab159d03fba0dbfc3268036905c
Adrian-Troya <wilmer.troya1680@utc.edu.ec>
2025-03-19 12:48:48 -0500

    Subiendo admin de los modelos

--- Aplicaciones/Agenda/admin.py
+++ Aplicaciones/Agenda/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Usuario, Categoria, Documento, AsignacionTiempo
+admin.site.register(Usuario)
+admin.site.register(Categoria)
+admin.site.register(Documento)
+admin.site.register(AsignacionTiempo)
 
 # Register your models here.
