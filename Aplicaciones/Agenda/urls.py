505c0c74d92f9977b4550630caa5cbc2213d5835
Adrian-Troya <wilmer.troya1680@utc.edu.ec>
2025-03-19 18:00:05 -0500

    Subiendo urls.py de Usuarios

--- Aplicaciones/Agenda/urls.py
+++ Aplicaciones/Agenda/urls.py
@@ -5,5 +5,12 @@ urlpatterns = [
     path('', views.login, name='login'),
     path('home/', views.home, name='home'),  # Ruta para la página principal
     path('reset/', views.reset, name='reset'),
-    # otras rutas...
+
+    # Rutas para los usuarios
+    path('listadoUsuarios/', views.listadoUsuarios, name='listadoUsuarios'),  # Listar usuarios
+    path('nuevoUsuario/', views.nuevoUsuario, name='nuevoUsuario'),  # Formulario nuevo usuario
+    path('guardarUsuario/', views.guardarUsuario, name='guardarUsuario'),  # Guardar nuevo usuario
+    path('editarUsuario/<int:id>/', views.editarUsuario, name='editarUsuario'),  # Formulario editar usuario
+    path('procesarActualizacionUsuario/', views.procesarActualizacionUsuario, name='procesarActualizacionUsuario'),  # Actualizar usuario
+    path('eliminarUsuario/<int:id>/', views.eliminarUsuario, name='eliminarUsuario'),  # Eliminar usuario
 ]
